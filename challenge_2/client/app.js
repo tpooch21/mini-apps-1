1f50a4f21042482bd4f8cb5ae2939d7c60ed3d73
Trevor Puccini <trevorpuccini21@gmail.com>
2020-04-21 11:30:06 -0700

    Initial set up of server express app, including installation of relevant dependencies

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,12 @@
+// Submits JSON data to the server
+// Receives response containing CSV-formatted result
+
+// Data should be submitted using an HTML form
+// Single textarea input field
+// POST the form data to the server
+
+// Craft form with method="post" and action="url"
+// Input text field
+// Input submit
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,26 @@
+// Build express app
+// Response from server should contain the CSV report along with the form so the user can keep submitting indefinitely
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const port = 3000;
+
+app.use(morgan('tiny'));
+// app.use(path('/'));
+
+app.listen(port, () => console.log(`App is listening on port ${port}`));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  console.log()
+  res.send('Hello World!')
+});
+
+
+
+
 
 
